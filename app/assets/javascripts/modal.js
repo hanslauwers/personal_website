0cf4b61e26e31673ca1c6b56c58046c17505ba4d
Hans Lauwers <hans.lauwers@hotmail.com>
2018-03-30 18:25:05 +0200

    Project images in modal

---
+++ app/assets/javascripts/modal.js
@@ -0,0 +1,16 @@
+function showModal(id){
+  var modal = document.getElementById('myModal');
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img = document.getElementById(id);
+  var modalImg = document.getElementById("modal-for-project");
+  var captionText = document.getElementById("caption");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+}
+
+function closeModal(){
+var modal = document.getElementById('myModal');
+  modal.style.display = "none";
+}
