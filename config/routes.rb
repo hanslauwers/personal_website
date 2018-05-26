7939d34527cb8e2fd8b07b33302a0a44cb10036e
Hans Lauwers <hans.lauwers@hotmail.com>
2018-05-25 18:40:03 -0700

    Remove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :technologies
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to => "home#index"
 
   get 'about', to: 'about#index'
   
-  resources :projects
+  #resources :technologies
+  #resources :projects
+  resources :projects, only: [:index]
 end
